c5c11a3aa640047a1a68236b61256c5d9545e8fa
986131@CS.MUM.EDU <986131@CS.MUM.EDU>
2017-10-13 15:20:51 -0500

    changes

--- src/main/java/edu/mum/se/poseidon/core/controllers/StudentController.java
+++ src/main/java/edu/mum/se/poseidon/core/controllers/StudentController.java
@@ -1,19 +1,27 @@
 package edu.mum.se.poseidon.core.controllers;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.mum.se.poseidon.core.services.StudentService;
+import edu.mum.se.poseidon.core.repositories.models.users.User;
 
 @Controller
 public class StudentController {
 
 	private StudentService studentService;
+	private static final Logger log = LoggerFactory.getLogger(StudentController.class);
 	
 	@Autowired
 	public StudentController(StudentService studentSerive){
@@ -21,10 +29,11 @@ public class StudentController {
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@RequestMapping(path="/students", method = RequestMethod.POST)
-	public ResponseEntity<?> create(@PathVariable String username, 
-				@PathVariable String password) {
-		return new ResponseEntity(this.studentService.createStudent(username, password),
+	@RequestMapping(path="/student/create", method = RequestMethod.PUT)
+	public ResponseEntity<?> create(@RequestBody User user) {
+		
+		log.info(user.getFirstName());
+		return new ResponseEntity(this.studentService.createStudent(user),
 				HttpStatus.NOT_FOUND);
 	}
 }
