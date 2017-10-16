aec04dcc42d7fa521287913125f68434d0039ea3
Enkh-Amgalan <eegii332002@yahoo.com>
2017-10-16 17:01:28 -0500

    Added classes for "Section" use case.

--- src/main/java/edu/mum/se/poseidon/core/services/SectionService.java
+++ src/main/java/edu/mum/se/poseidon/core/services/SectionService.java
@@ -1,8 +1,12 @@
 package edu.mum.se.poseidon.core.services;
 
+import edu.mum.se.poseidon.core.controllers.PoseidonException;
 import edu.mum.se.poseidon.core.repositories.SectionRepository;
+import edu.mum.se.poseidon.core.repositories.models.Section;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SectionService {
@@ -12,5 +16,36 @@ public class SectionService {
     @Autowired
     public SectionService(SectionRepository sectionRepository) {
         this.sectionRepository = sectionRepository;
+    }
+
+    public Section createSection(Section section) {
+        return sectionRepository.save(section);
+    }
+
+    public void deleteSection(Long id) throws PoseidonException {
+        Section section = sectionRepository.getOne(id);
+        if (section == null) {
+            throw new PoseidonException("Section is not found with this id.");
+        } else {
+            section.setDeleted(true);
+            sectionRepository.save(section);
+        }
+    }
+
+    public Section editSection(Section section) throws PoseidonException {
+        if (sectionRepository.exists(section.getId())) {
+            sectionRepository.save(section);
+            return section;
+        } else {
+            throw new PoseidonException("Section is not found with this id.");
+        }
+    }
+
+    public List<Section> getSectionList() {
+        return sectionRepository.findSectionsByDeleted(false);
+    }
+
+    public Section getSection(Long id) {
+        return sectionRepository.findOne(id);
     }
 }
