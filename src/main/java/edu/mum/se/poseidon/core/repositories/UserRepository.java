70d81e4224c412b5d0505cb2aae95bd22ad802c8
Sophoana Chlang <chlang2403@gmail.com>
2017-10-16 15:52:11 -0500

    fixed deleted-user can login

--- src/main/java/edu/mum/se/poseidon/core/repositories/UserRepository.java
+++ src/main/java/edu/mum/se/poseidon/core/repositories/UserRepository.java
@@ -5,17 +5,20 @@ import edu.mum.se.poseidon.core.repositories.models.users.User;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by Yuriy Yugay on 10/11/2017.
  *
  * @author Yuriy Yugay
  */
-public interface UserRepository
-        extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByUsername(String username);
+	@Query("select u from User u where u.username= :username and u.deleted = false ")
+	User findUserByUsername(@Param("username") String username);
 
-    List<User> findAllByDeleted(boolean is_deleted);
-    
+	List<User> findAllByDeleted(boolean is_deleted);
+
+	
 }
