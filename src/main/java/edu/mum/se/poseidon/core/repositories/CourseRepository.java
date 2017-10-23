2228cb6e9a603787dc92d4cf7ff9b911d12fbb0a
Sophoana Chlang <chlang2403@gmail.com>
2017-10-23 00:51:30 -0500

    Update Schedule to handle "assignFacultyToSection" Randomly

--- src/main/java/edu/mum/se/poseidon/core/repositories/CourseRepository.java
+++ src/main/java/edu/mum/se/poseidon/core/repositories/CourseRepository.java
@@ -11,7 +11,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    @Query("select c from Course c where c.id = :sectionId")
+    //@Query("select c from Course c where c.id = :sectionId")
+    //@Query("select s from Section s inner join Course on Course.id = s.course_id where s.id = :sectionId")
+    @Query("select c, s from Course c inner  join  c.sections s where s.id = :sectionId")
     Course findCourseBySectionId(@Param("sectionId") Long sectionId);
 
     List<Course> findAllByDeleted(boolean is_deleted);
@@ -19,4 +21,5 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     Set<Course> findByIdIn(Set<Long> courseIds);
 
     Course findCourseByNumber(int number);
+
 }
--- src/main/java/edu/mum/se/poseidon/core/repositories/FacultyRepository.java
+++ src/main/java/edu/mum/se/poseidon/core/repositories/FacultyRepository.java
@@ -2,13 +2,21 @@ package edu.mum.se.poseidon.core.repositories;
 
 import java.util.List;
 
+import edu.mum.se.poseidon.core.controllers.dto.FacultySectionDto;
+import jdk.nashorn.internal.runtime.FindProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.mum.se.poseidon.core.repositories.models.users.Faculty;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface FacultyRepository extends  JpaRepository<Faculty, Long>{
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
 
-	List<Faculty> findAllByDeleted(boolean is_deleted);
+    List<Faculty> findAllByDeleted(boolean is_deleted);
 
-	Faculty findByFirstNameEquals(String firstname);
+    Faculty findByFirstNameEquals(String firstname);
+
+    @Query("select f, c from Faculty f inner  join  f.courses c where c.id = :courseId")
+    List<Faculty> findFacultiesByCourseId(@Param("courseId") Long courseId);
+
 }
